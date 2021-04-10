aae70a7f03d0cf9c2e579ae37c8c9f1070ecf4e0
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-04-10 11:44:15 +0300

    data-str

--- lessons/33-data-structure/index.js
+++ lessons/33-data-structure/index.js
@@ -1,21 +1,80 @@
 const quickSort = array => {
-  return array
+  const arr = [...array]
+  if (arr.length < 2) return arr
+  const pivot = arr[0]
+  const less = arr.filter((item) => item.order < pivot.order)
+  const more = arr.filter((item) => item.order > pivot.order)
+  return [...quickSort(less), pivot, ...quickSort(more)]
 }
 
 const toFlat = array => {
-  return array
+  const stack = [...array]
+  const result = []
+  while (stack.length) {
+    const deeper = stack.pop()
+    if (Array.isArray(deeper)) {
+      stack.push(...deeper)
+    } else {
+      result.push(deeper)
+    }
+  }
+  return result.reverse()
 }
 
 const binarySearch = (array, searchValue) => {
-  return 0
+  let start = 0
+  let end = array.length
+  let middle
+  let found = false
+  let position = -1
+  while (found === false && start <= end) {
+    middle = Math.floor((start + end) / 2)
+    if (array[middle].amount === searchValue) {
+      found = true
+      position = middle
+      return array[position]
+    }
+    if (searchValue < array[middle].amount) {
+      end = middle - 1
+    }
+    else {
+      start = middle + 1
+    }
+  }
+  return position
 }
 
 class HashTable {
-  constructor () {
-    this.map = {}
+  static getKeyByName(name) {
+    return name.charCodeAt(0)
   }
-  add (value) {}
-  find (name) {}
+  constructor() {
+    this.map = new Map()
+  }
+  add(value) {
+    const key = HashTable.getKeyByName(value.name)
+    if (!this.map.has(key)) {
+      this.map.set(key, [])
+    }
+    this.map.get(key).push(value)
+    return key
+  }
+  find(name) {
+    const key = HashTable.getKeyByName(name)
+    const employeesMap = this.map.get(key)
+    if (!employeesMap) {
+      return
+    }
+    return employeesMap.find(item => item.name === name)
+  }
+  removeByName(name) {
+    const key = HashTable.getKeyByName(name)
+    let employeesMap = this.map.get(key)
+    if (!employeesMap) {
+      return
+    }
+    this.map.set(key, employeesMap.filter(item => item.name !== name))
+  }
 }
 
 module.exports = {
