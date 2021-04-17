40893f22374abfc3e7bf10526c8aa5796dc9ade3
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-04-17 14:01:53 +0300

    35/npm

--- lessons/35-npm/public-package/index.js
+++ lessons/35-npm/public-package/index.js
@@ -1,6 +1,9 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
 
+const notVerySmartSearch = (array, value) => array.filter((item) => item === value)
+
 module.exports = {
   smartSearch,
+  notVerySmartSearch,
 }
